f1a9cdb677bad30155f51826ebca51591d699c9e
Roy Abitbol <roy.abitbol@il.ibm.com>
2017-01-10 00:19:55 +0200

    added many filters and progressed towards ridge index

---
+++ pymorph-0.96/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+try:
+    import setuptools
+except ImportError:
+    import sys
+    print >>sys.stderr, '''\
+Could not import `setuptools` module.
+
+Please install it.
+
+Under Ubuntu, it is in a package called `python-setuptools`.'''
+    sys.exit(1)
+
+from setuptools import setup, find_packages
+exec(open('pymorph/pymorph_version.py').read())
+# execfile('pymorph/pymorph_version.py')
+
+long_description = "" #file('README.rst').read()
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering :: Image Recognition',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+
+setup(name='pymorph',
+      version=__version__,
+      description='Image Morphology Toolbox',
+      long_description=long_description,
+      author='Luis Pedro Coelho',
+      author_email='lpc@cmu.edu',
+      url='http://luispedro.org/software/pymorph/',
+      license='BSD',
+      packages=find_packages(),
+      )
